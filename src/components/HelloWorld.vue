5d742cdb6e7cd4461f45808ee220e5420acf386c
annin3 <shunta.titech@gmail.com>
2020-12-31 14:08:09 +0900

    deleate readme

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,17 +12,8 @@
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
+          杏仁^3's portfolio site
         </h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
       </v-col>
 
       <v-col
