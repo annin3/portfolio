b9f968e8aa7ea2ffb67906199fb55d4392447a8d
annin3 <shunta.titech@gmail.com>
2021-01-12 02:20:08 +0900

    header, router, icon

---
+++ src/components/Navigation/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app-bar
+    app
+    color="rgba(255, 255, 255, 0)"
+    dense="boolean"
+    flat="boolean"
+  >
+    <v-app-bar-nav-icon v-on:click="drawer = true"></v-app-bar-nav-icon>
+    <v-spacer></v-spacer>
+    <div>English</div>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Emit } from 'vue-property-decorator'
+
+@Component({
+  name: 'AppBar'
+})
+
+export default class AppBar extends Vue {
+}
+</script>
+
+<style lang="sass" scoped>
+</style>
